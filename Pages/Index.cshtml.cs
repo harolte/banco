dd2cd5921319a06429695b2f95e862920d9e370b
harolte <harolte@hotmail.com>
2023-03-24 23:31:05 -0500

    se agrega el proycto

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace banco.Pages;
+
+
+public class IndexModel : PageModel
+
+    {
+
+        public void OnGet()
+
+        {
+
+        }
+
+ 
+
+        public void OnPost(DateTime fecha, string monto)
+
+        {
+
+            double total,moras;
+            
+            double resultado = 0;
+           
+            DateTime fechaActual = DateTime.Now;
+
+            TimeSpan diferencia = fechaActual - fecha;
+            
+            int diasTranscurridos = diferencia.Days;                  
+
+            double mont = double.Parse(monto);
+
+            moras = diasTranscurridos * (mont*0.005);
+            
+            resultado = mont + moras;
+
+           
+
+            ViewData["resultado"] = resultado;
+
+        }
+
+    }
+
+
